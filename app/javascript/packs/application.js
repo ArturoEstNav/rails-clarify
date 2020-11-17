942af3e97eb26507fff7d607d168ec7b589de647
Arturo Estrada <arturoestradanav@gmail.com>
2020-11-17 15:19:06 -0600

    added cloudinary js configuration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// responsive sources
+
+<script src="node_modules/lodash/lodash.js"                   type="text/javascript"></script>
+<script src="node_modules/cloudinary-core/cloudinary-core.js" type="text/javascript"></script>
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -33,4 +37,13 @@ document.addEventListener('turbolinks:load', () => {
   initMapbox();
 });
 
-// Responsiveness
+// Responsive config
+
+<script type="text/javascript">
+    var cl = cloudinary.Cloudinary.new({cloud_name: "dknmmwaff"});
+    // replace 'demo' with your cloud name in the line above
+    cl.responsive();
+</script>
+
+
+
