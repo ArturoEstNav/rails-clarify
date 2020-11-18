fa9bd2ac9de7e90377b2051c1d4d14159c6a613b
Arturo Estrada <arturoestradanav@gmail.com>
2020-11-17 22:09:49 -0600

    installed cloudinare from npm again to try to correct a missing var

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+
 // responsive sources
 
-<script src="node_modules/lodash/lodash.js"                   type="text/javascript"></script>;
-<script src="node_modules/cloudinary-core/cloudinary-core.js" type="text/javascript"></script>
+// <script src="node_modules/lodash/lodash.js"                   type="text/javascript"></script>;
+// <script src="node_modules/cloudinary-core/cloudinary-core.js" type="text/javascript"></script>
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -29,6 +30,9 @@ require("channels")
 // External imports
 import "bootstrap";
 
+import "cloudinary";
+import "cloudinary-core";
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from './map';
@@ -38,12 +42,7 @@ document.addEventListener('turbolinks:load', () => {
 });
 
 // Responsive config
-
-// <script type="text/javascript">
-    var cl = cloudinary.Cloudinary.new({cloud_name: "dknmmwaff"});
-    // replace 'demo' with your cloud name in the line above
-    cl.responsive();
-// </script>
-
+  const cl = cloudinary.Cloudinary.new({cloud_name: "dknmmwaff"});
+  cl.responsive();
 
 
